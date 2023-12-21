21e1347bb25b3131667e847adf01332ea2e7459a
lexeach <80155340+lexeach@users.noreply.github.com>
2023-12-21 16:28:59 +0530

    Update home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -898,7 +898,7 @@ const Dashboard = () => {
                         <option value="24">24</option>
                         <option value="36">36</option>
                         <option value="48">48</option>
-                        <option value="60">60</option>
+                        
                       </select>
                       {loading && (
                         <div className="loader-overlay">
